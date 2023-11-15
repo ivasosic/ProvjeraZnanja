5dfd48d17995acb326b9a3a80b9e0ec530e980b7
a <145933200+ivasosic@users.noreply.github.com>
2023-11-15 09:28:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvjeraZnanja2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napišite program koji će zatražiti od korisnika da unese dva stringa, te će ih ispisati po 
+            //abecedi u uzlaznom redoslijedu.
+            Console.WriteLine("Unesite 1. string: ");
+            string s1 = Console.ReadLine();
+            Console.Write("\nUnesite 2. string: \n");
+            string s2 = Console.ReadLine();
+
+
+
+        }
+    }
+}
